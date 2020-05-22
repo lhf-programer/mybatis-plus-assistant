3daf6fbe531010e6d98f49a7f2289359af027519
lvhaifeng <1179438473@qq.com>
2020-05-22 18:14:59 +0800

    初始化

---
+++ src/test/java/com/lvhaifeng/mybatis/MybatisPlusAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvhaifeng.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
