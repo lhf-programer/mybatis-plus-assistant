ca4fda37c5a3253de21ade867559ca8c0b1e288e
lvhaifeng <1179438473@qq.com>
2020-05-26 16:11:49 +0800

    v1.2 构建

--- src/main/java/com/lvhaifeng/mybatis/query/QueryWrapperExt.java
+++ src/main/java/com/lvhaifeng/mybatis/query/QueryWrapperExt.java
@@ -85,7 +85,7 @@ public class QueryWrapperExt<T> extends QueryWrapper<T> {
 
             ColumnOriginal fieldColumnOriginal = field.getAnnotation(ColumnOriginal.class);
             if (null == fieldColumnOriginal) {
-                this.resultFields.add(ConvertUtils.camelToUnderline(field.getName()) + "as" + field.getName());
+                this.resultFields.add(ConvertUtils.camelToUnderline(field.getName()) + " as " + field.getName());
             } else {
                 String column = fieldColumnOriginal.value();
                 this.resultFields.add(column + " as " + field.getName());
